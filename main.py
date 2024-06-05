1dba0f1e5d9252b71ed6051b177a3052e9ee6f3f
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2024-06-06 02:50:26 +0900

    a

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if(st.button("시 작성")):
         res = chatModel.invoke(sub + "에서 시의 주제를 추출해. 추출한 주제에 관한 시를 하나 써줘.\우선 시를 작성해.\작성한 시를 10점 만점으로 평가해.\만약 시가 8점을 넘지 못했다면 다시 생성해.\8점이 넘은 시를 결과로 해.\json 형식으로 시도 횟수(attempts), 제목(head), 내용(body), 점수(rating)와 간단한 분석(reason)을 출력해.\시에서 줄바꿈은 <br>로 처리해.\프롬프트에 적힌 내용을 echo하지 말것.")
         print(res.content)
         #try:
-        response_json = res.content.json()
+        response_json = json.loads(res.content)
 
         st.markdown(
             f"""
