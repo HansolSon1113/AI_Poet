def0611f50cb8df57be49a9e187ef0421941fecc
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2024-06-06 02:15:33 +0900

    json

--- main.py
+++ main.py
@@ -3,24 +3,24 @@
 #from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 import streamlit as st
-
+import json
 
 #Settings
 st.title('AI Poem Writer')
 #load_dotenv()
 chatModel = ChatOpenAI()
 
-
 #Main
 sub = st.text_input('시의 주제를 입력하세요: ')
-st.markdown("<p style='color:Gray; font-size: 13px;>시의 주제: ", sub, "</p>")
+st.markdown(f"<p style='color:Gray; font-size: 13px;'>시의 주제: {sub}</p>")
 
 if(st.button("시 작성")):
     with st.spinner("시 작성중 ..."):
         res = chatModel.invoke(sub + "밤에 관한 시를 써줘.\우선 시를 작성해.\작성한 시를 10점 만점으로 평가해.\만약 시가 8점을 넘지 못했다면 다시 생성해.\8점이 넘은 시를 결과로 해.\json 형식으로 시도 횟수(attempts), 제목(head), 내용(body), 점수(rating)와 간단한 분석(reason)을 출력해.")
-        st.markdown("<p style='color:Blue; font-size: 20px;'>", res.content[head], "\n</p>",
-                    "<p style='color:Black; font-size: 15px;'>", res.content[body], "\n</p>",
-                    "<p style='color:Violet; font-size: 15px;'>", res.content[attempts], "\n</p>",
-                    "<p style='color:Red; font-size: 15px;'>", res.content[rating], "\n</p>",
-                    "<p style='color:Green; font-size: 15px;'>", res.content[reason], "\n</p>"
-                    )
+        response_json = json.loads(res.content)
+        
+        st.markdown(f"<p style='color:Blue; font-size: 20px;'>{response_json['head']}</p>")
+        st.markdown(f"<p style='color:Black; font-size: 15px;'>{response_json['body']}</p>")
+        st.markdown(f"<p style='color:Violet; font-size: 15px;'>시도 횟수: {response_json['attempts']}</p>")
+        st.markdown(f"<p style='color:Red; font-size: 15px;'>점수: {response_json['rating']}</p>")
+        st.markdown(f"<p style='color:Green; font-size: 15px;'>분석: {response_json['reason']}</p>")
